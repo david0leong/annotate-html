7bd9818a923d0a859702318acef481b8999b7a7a
David Leong <david0leong@gmail.com>
2020-03-01 20:21:32 -0800

    Implement annotate util(WIP)

--- routes/annotate.js
+++ routes/annotate.js
@@ -8,7 +8,19 @@ const router = express.Router();
 router.post('/', function(req, res, next) {
   const annotations = annotationsDataSource.getAll();
 
-  res.send(annotate(req.body, annotations));
+  // res.send(annotate(req.body, annotations));
+  res.send(
+    annotate(req.body, [
+      {
+        name: 'alex',
+        url: 'http://alex.com',
+      },
+      {
+        name: 'bo',
+        url: 'http://bo.com',
+      },
+    ])
+  );
 });
 
 module.exports = router;
--- utils/annotate.js
+++ utils/annotate.js
@@ -1,7 +1,29 @@
 const cheerio = require('cheerio');
 
+const annotateText = (text, annotations) =>
+  annotations.reduce(
+    (acc, { name, url }) =>
+      acc.replace(
+        new RegExp(`\\b${name}\\b`, 'gi'),
+        match => `<a href="${url}">${match}</a>`
+      ),
+    text
+  );
+
+const annotateElement = (el, annotateFn) => {
+  if (el.type === 'text') {
+    el.data = annotateFn(el.data);
+  } else if (el.type === 'tag' && el.name !== 'a') {
+    el.children.forEach(childEl => {
+      annotateElement(childEl, annotateFn);
+    });
+  }
+};
+
 const annotate = (html, annotations) => {
   const $ = cheerio.load(html);
+
+  annotateElement($('body')[0], text => annotateText(text, annotations));
 
   return $.html();
 };
